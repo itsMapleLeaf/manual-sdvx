bb966b6b29c08cbf20068b7fa8432c7123b02b96
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-09 17:15:07 -0500

    clean up song fetcher

--- scripts/fetch_songs.py
+++ scripts/fetch_songs.py
@@ -1,78 +1,85 @@
-import json
+import asyncio
 import os
 from pathlib import Path
-from urllib import request
+import aiohttp
 from bs4 import BeautifulSoup, Tag
 
 from .songs import Song
 
-if __name__ == "__main__":
-    PAGE_COUNT = 53
+
+async def fetch_all_songs(page_count: int):
+    print(f"fetching all songs from {page_count} pages...")
 
     songs: list[Song] = []
 
-    for page_number in range(1, PAGE_COUNT + 1):
-        url = f"https://www.myshkin.io/sdvx/songlist?page={page_number}"
-        print(f"fetching {url}")
+    async with aiohttp.ClientSession() as session:
+        tasks = [
+            fetch_page_songs(session, page_index + 1)
+            for page_index in range(page_count)
+        ]
+        for page_songs in await asyncio.gather(*tasks):
+            songs += page_songs
 
-        req = request.urlopen(url)
-        html_content = req.read().decode("utf-8")
-        soup = BeautifulSoup(html_content, "html5lib")
+    return songs
 
-        for row in soup.select("#song-table-body > tr"):
-            title_element = row.select_one(".song-title")
-            title = (
-                title_element.get_text(strip=True) if title_element else "Unknown Title"
-            )
 
-            artist_element = row.select_one(".song-artist")
-            artist = (
-                artist_element.get_text(strip=True)
-                if artist_element
-                else "Unknown Artist"
-            )
+async def fetch_page_songs(session: aiohttp.ClientSession, page_number: int):
+    url = f"https://www.myshkin.io/sdvx/songlist?page={page_number}"
 
-            groups = ["Unknown Group"]
-            genre_header = None
-            for header in row.select(".metadata-header"):
-                if "Genre" in header.get_text():
-                    genre_header = header
-                    break
+    async with session.get(url) as response:
+        html_content = await response.text()
 
-            if genre_header:
-                next_sibling = genre_header.find_next_sibling()
-                if next_sibling:
-                    groups = [
-                        part.strip() for part in next_sibling.get_text().split(",")
-                    ]
+    print(f"fetched page {page_number}")
 
-            charts: dict[str, int] = {}
-            for cell in row.select(".chart-difficulty"):
-                cell_text = cell.get_text(strip=True)
-                parts = cell_text.split(" ")
-                if len(parts) >= 2:
-                    key = parts[0]
-                    level = parts[1]
-                    try:
-                        charts[key] = int(level)
-                    except ValueError:
-                        print(f"failed to parse difficulty cell: {cell_text}")
-                        continue
-                else:
-                    print(f"failed to parse difficulty cell: {cell_text}")
-                    continue
+    soup = BeautifulSoup(html_content, "html5lib")
+    return [parse_song(row) for row in soup.select("#song-table-body > tr")]
 
-            songs += [
-                Song(
-                    identifier=f"{title} by {artist}",
-                    title=title,
-                    artist=artist,
-                    groups=groups,
-                    charts=charts,
-                )
-            ]
 
-        print(f"found {len(songs)} songs...")
+def parse_song(row: Tag):
+    title_element = row.select_one(".song-title")
+    title = title_element.get_text(strip=True) if title_element else "Unknown Title"
+
+    artist_element = row.select_one(".song-artist")
+    artist = artist_element.get_text(strip=True) if artist_element else "Unknown Artist"
+
+    groups = ["Unknown Group"]
+    genre_header = None
+    for header in row.select(".metadata-header"):
+        if "Genre" in header.get_text():
+            genre_header = header
+            break
+
+    if genre_header and (next_sibling := genre_header.find_next_sibling()):
+        groups = [part.strip() for part in next_sibling.get_text().split(",")]
+
+    charts: dict[str, int] = {}
+    for cell in row.select(".chart-difficulty"):
+        cell_text = cell.get_text(strip=True)
+        parts = cell_text.split(" ")
+        if len(parts) >= 2:
+            key = parts[0]
+            level = parts[1]
+            try:
+                charts[key] = int(level)
+            except ValueError:
+                print(f"failed to parse difficulty cell: {cell_text}")
+                continue
+        else:
+            print(f"failed to parse difficulty cell: {cell_text}")
+            continue
+
+    return Song(
+        identifier=f"{title} by {artist}",
+        title=title,
+        artist=artist,
+        groups=groups,
+        charts=charts,
+    )
+
+
+if __name__ == "__main__":
+    songs = asyncio.run(fetch_all_songs(53))
+    songs.sort(key=lambda song: song.title)
 
     print(f"saving {len(songs)} songs...")
 
@@ -81,11 +88,8 @@ if __name__ == "__main__":
 
     os.makedirs(data_dir, exist_ok=True)
     with open(songs_file_path, "w", encoding="utf-8") as file:
-        json.dump(
-            [song.to_dict() for song in songs],
-            file,
-            indent=4,
-            ensure_ascii=False,
+        file.write(
+            Song.schema().dumps(songs, many=True, indent="\t", ensure_ascii=False)
         )
 
     print("done")
