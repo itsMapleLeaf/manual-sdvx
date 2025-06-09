584dd9f88b629909c642ce44b23bed1d539beb43
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-09 17:33:59 -0500

    clean up (?) build script

--- scripts/build.py
+++ scripts/build.py
@@ -42,228 +42,231 @@ def navigator_key_category_for(navigator: str) -> str:
     return f"Navigator Access for {navigator}"
 
 
-if __name__ == "__main__":
-    locations: List[Location] = [
-        {
-            "name": "PERFECT ULTIMATE CHAIN",
-            "requires": "|@Boss Clear|",
-            "victory": True,
-            "category": ["((Victory))"],
-        }
-    ]
+class SoundVoltexWorld:
+    locations: list[Location]
+    items: list[Item]
+    categories: dict[str, Category]
 
-    items = [
-        Item(
-            name="CHAIN",
-            category=["CHAIN"],
-            progression=True,
-            count=20,
-            value={"chain": 1},
-        ),
-        Item(
-            name="5 CHAIN",
-            category=["CHAIN"],
-            progression=True,
-            count=10,
-            value={"chain": 5},
-        ),
-        Item(
-            name="10 CHAIN",
-            category=["CHAIN"],
-            progression=True,
-            count=5,
-            value={"chain": 10},
-        ),
-        Item(
-            name="20 CHAIN",
-            category=["CHAIN"],
-            progression=True,
-            count=3,
-            value={"chain": 20},
-        ),
-        Item(
-            name="50 CHAIN",
-            category=["CHAIN"],
-            progression=True,
-            count=2,
-            value={"chain": 50},
-        ),
-        Item(
-            name="100 CHAIN",
-            category=["CHAIN"],
-            progression=True,
-            count=1,
-            value={"chain": 100},
-        ),
-    ]
-
-    categories = {
-        "Goals": Category(hidden=True),
-        "Consumables": Category(hidden=True),
-    }
-
-    for navigator in navigators:
-        items.append(
-            Item(
-                name=f"{navigator} [NAVIGATOR ACCESS]",
-                progression=True,
-                category=["Navigator Keys", navigator_key_category_for(navigator)],
-            )
-        )
-        categories[navigator_key_category_for(navigator)] = Category(hidden=True)
-
-    for song_number, song in enumerate(ALL_SONGS):
-        categories[song.identifier] = Category(hidden=True)
-        categories[song_number_category_for(song_number)] = Category(hidden=True)
-
-        song_navigators = [
-            navigator
-            for navigator, navigator_songs in navigators.items()
-            if song.title in navigator_songs
+    def __init__(self) -> None:
+        self.locations = [
+            {
+                "name": "PERFECT ULTIMATE CHAIN",
+                "requires": "|@Boss Clear|",
+                "victory": True,
+                "category": ["((Victory))"],
+            }
         ]
 
-        goals = ["Pass", "AA Rank", "AAA Rank", "S Rank"]
-        is_boss = any(level >= 20 for level in song.charts.values())
+        self.items = [
+            Item(
+                name="CHAIN",
+                category=["CHAIN"],
+                progression=True,
+                count=20,
+                value={"chain": 1},
+            ),
+            Item(
+                name="5 CHAIN",
+                category=["CHAIN"],
+                progression=True,
+                count=10,
+                value={"chain": 5},
+            ),
+            Item(
+                name="10 CHAIN",
+                category=["CHAIN"],
+                progression=True,
+                count=5,
+                value={"chain": 10},
+            ),
+            Item(
+                name="20 CHAIN",
+                category=["CHAIN"],
+                progression=True,
+                count=3,
+                value={"chain": 20},
+            ),
+            Item(
+                name="50 CHAIN",
+                category=["CHAIN"],
+                progression=True,
+                count=2,
+                value={"chain": 50},
+            ),
+            Item(
+                name="100 CHAIN",
+                category=["CHAIN"],
+                progression=True,
+                count=1,
+                value={"chain": 100},
+            ),
+        ]
 
-        if is_boss:
-            items.append(
+        self.categories = {
+            "Goals": Category(hidden=True),
+            "Consumables": Category(hidden=True),
+        }
+
+        for navigator in navigators:
+            self.items.append(
                 Item(
-                    name=song.identifier,
+                    name=f"{navigator} [NAVIGATOR ACCESS]",
                     progression=True,
-                    category=[
-                        "Goals",
-                        song.identifier,
-                        song_number_category_for(song_number),
-                        "Boss Access",
-                    ],
+                    category=["Navigator Keys", navigator_key_category_for(navigator)],
                 )
             )
-            locations.append(
-                Location(
-                    name=song.identifier,
-                    requires=f"{{ItemValue(chain:300)}} and |@{song_number_category_for(song_number)}|",
-                    category=[
-                        "Goals",
-                        song.identifier,
-                        f"(Boss) {song.identifier}",
-                    ],
-                    place_item=[f"{song.identifier} (Completion)"],
-                )
+            self.categories[navigator_key_category_for(navigator)] = Category(
+                hidden=True
             )
-            items.append(
-                Item(
-                    name=f"{song.identifier} (Completion)",
-                    progression=True,
-                    category=["Goals", song.identifier, "Boss Clear"],
-                )
+
+        for song_number, song in enumerate(ALL_SONGS):
+            self.categories[song.identifier] = Category(hidden=True)
+            self.categories[song_number_category_for(song_number)] = Category(
+                hidden=True
             )
-        elif song_navigators:
-            for goal in goals:
-                locations.append(
-                    Location(
-                        name=f"{song.identifier} ({goal})",
-                        requires=" or ".join(
-                            f"|@{navigator_key_category_for(navigator)}|"
-                            for navigator in song_navigators
-                        ),
+
+            song_navigators = [
+                navigator
+                for navigator, navigator_songs in navigators.items()
+                if song.title in navigator_songs
+            ]
+
+            goals = ["Pass", "AA Rank", "AAA Rank", "S Rank"]
+            is_boss = any(level >= 20 for level in song.charts.values())
+
+            if is_boss:
+                self.items.append(
+                    Item(
+                        name=song.identifier,
+                        progression=True,
                         category=[
                             "Goals",
                             song.identifier,
-                            f"(Song) {song.identifier}",
-                            f"(Goal) {goal}",
-                            *[
-                                f"(Navigator) {navigator}"
+                            song_number_category_for(song_number),
+                            "Boss Access",
+                        ],
+                    )
+                )
+                self.locations.append(
+                    Location(
+                        name=song.identifier,
+                        requires=f"{{ItemValue(chain:300)}} and |@{song_number_category_for(song_number)}|",
+                        category=[
+                            "Goals",
+                            song.identifier,
+                            f"(Boss) {song.identifier}",
+                        ],
+                        place_item=[f"{song.identifier} (Completion)"],
+                    )
+                )
+                self.items.append(
+                    Item(
+                        name=f"{song.identifier} (Completion)",
+                        progression=True,
+                        category=["Goals", song.identifier, "Boss Clear"],
+                    )
+                )
+            elif song_navigators:
+                for goal in goals:
+                    self.locations.append(
+                        Location(
+                            name=f"{song.identifier} ({goal})",
+                            requires=" or ".join(
+                                f"|@{navigator_key_category_for(navigator)}|"
                                 for navigator in song_navigators
+                            ),
+                            category=[
+                                "Goals",
+                                song.identifier,
+                                f"(Song) {song.identifier}",
+                                f"(Goal) {goal}",
+                                *[
+                                    f"(Navigator) {navigator}"
+                                    for navigator in song_navigators
+                                ],
                             ],
-                        ],
+                        )
                     )
-                )
-        else:
-            items.append(
-                Item(
-                    name=song.identifier,
-                    progression=True,
-                    category=[
-                        "Songs",
-                        song.identifier,
-                        song_number_category_for(song_number),
-                    ],
-                )
-            )
-            for goal in goals:
-                locations.append(
-                    Location(
-                        name=f"{song.identifier} ({goal})",
-                        requires=f"|@{song_number_category_for(song_number)}|",
+            else:
+                self.items.append(
+                    Item(
+                        name=song.identifier,
+                        progression=True,
                         category=[
-                            "Goals",
+                            "Songs",
                             song.identifier,
-                            f"(Song) {song.identifier}",
-                            f"(Goal) {goal}",
+                            song_number_category_for(song_number),
                         ],
                     )
                 )
+                for goal in goals:
+                    self.locations.append(
+                        Location(
+                            name=f"{song.identifier} ({goal})",
+                            requires=f"|@{song_number_category_for(song_number)}|",
+                            category=[
+                                "Goals",
+                                song.identifier,
+                                f"(Song) {song.identifier}",
+                                f"(Goal) {goal}",
+                            ],
+                        )
+                    )
 
-    items += [
-        Item(name="Swap Lazer Colors", count=3, trap=True, category=["Traps"]),
-        Item(name="Hard Timing Window", count=3, trap=True, category=["Traps"]),
-        Item(name="Rate +1.1", count=3, trap=True, category=["Traps"]),
-        Item(name="Random", count=3, trap=True, category=["Traps"]),
-        Item(name="Pass Latest Nautica Chart", count=3, trap=True, category=["Traps"]),
-        Item(name="Pass a 20", count=3, trap=True, category=["Traps"]),
-        Item(name="Slowjam (Speed 3.0)", count=3, trap=True, category=["Traps"]),
-        Item(name="Speedjam (Speed 9.0)", count=3, trap=True, category=["Traps"]),
-        Item(name="Score +5.0000", count=20, useful=True, category=["Helpers"]),
-        Item(name="Score +10.0000", count=10, useful=True, category=["Helpers"]),
-        Item(name="Score +20.0000", count=5, useful=True, category=["Helpers"]),
-        Item(name="Score +50.0000", count=3, useful=True, category=["Helpers"]),
-        Item(name="Score +100.0000", count=1, useful=True, category=["Helpers"]),
-        Item(name="Cancel Trap", count=12, useful=True, category=["Helpers"]),
-        Item(name="Downlevel", count=12, useful=True, category=["Helpers"]),
-    ]
+        self.items += [
+            Item(name="Swap Lazer Colors", count=3, trap=True, category=["Traps"]),
+            Item(name="Hard Timing Window", count=3, trap=True, category=["Traps"]),
+            Item(name="Rate +1.1", count=3, trap=True, category=["Traps"]),
+            Item(name="Random", count=3, trap=True, category=["Traps"]),
+            Item(
+                name="Pass Latest Nautica Chart", count=3, trap=True, category=["Traps"]
+            ),
+            Item(name="Pass a 20", count=3, trap=True, category=["Traps"]),
+            Item(name="Slowjam (Speed 3.0)", count=3, trap=True, category=["Traps"]),
+            Item(name="Speedjam (Speed 9.0)", count=3, trap=True, category=["Traps"]),
+            Item(name="Score +5.0000", count=20, useful=True, category=["Helpers"]),
+            Item(name="Score +10.0000", count=10, useful=True, category=["Helpers"]),
+            Item(name="Score +20.0000", count=5, useful=True, category=["Helpers"]),
+            Item(name="Score +50.0000", count=3, useful=True, category=["Helpers"]),
+            Item(name="Score +100.0000", count=1, useful=True, category=["Helpers"]),
+            Item(name="Cancel Trap", count=12, useful=True, category=["Helpers"]),
+            Item(name="Downlevel", count=12, useful=True, category=["Helpers"]),
+        ]
 
-    gauge_levels = [
-        # start at Blastive 2.5,
-        "Blastive 2.0",
-        "Blastive 1.5",
-        "Blastive 1.0",
-        "Blastive 0.5",
-        "Effective",
-    ]
+        gauge_levels = [
+            # start at Blastive 2.5,
+            "Blastive 2.0",
+            "Blastive 1.5",
+            "Blastive 1.0",
+            "Blastive 0.5",
+            "Effective",
+        ]
 
-    items += [
-        Item(
-            name="Progressive Gauge",
-            count=len(gauge_levels) + 3,
-            progression=True,
-            category=["Progressive Gauge"],
-        )
-    ]
-
-    for index, rate in enumerate(gauge_levels):
-        locations.append(
-            Location(
-                name=f"Progressive Gauge ({rate})",
-                requires=f"|Progressive Gauge:{index + 1}|",
-                category=["((Helpers)) Progressive Gauge"],
+        self.items += [
+            Item(
+                name="Progressive Gauge",
+                count=len(gauge_levels) + 3,
+                progression=True,
+                category=["Progressive Gauge"],
             )
-        )
+        ]
 
-    total_count = sum(item.get("count") or 1 for item in items)
-    print(f"Generated {total_count} items")
-    print(f"Generated {len(locations)} locations")
-    print(f"Configured {len(categories)} categories")
+        for index, rate in enumerate(gauge_levels):
+            self.locations.append(
+                Location(
+                    name=f"Progressive Gauge ({rate})",
+                    requires=f"|Progressive Gauge:{index + 1}|",
+                    category=["((Helpers)) Progressive Gauge"],
+                )
+            )
 
-    with open(manual_data_path("items.json"), "w", encoding="utf-8") as file:
-        json.dump(items, file, indent=4, ensure_ascii=False)
+    @property
+    def item_count(self):
+        return sum(item.get("count") or 1 for item in world.items)
 
-    with open(manual_data_path("locations.json"), "w", encoding="utf-8") as file:
-        json.dump(locations, file, indent=4, ensure_ascii=False)
 
-    with open(manual_data_path("categories.json"), "w", encoding="utf-8") as file:
-        json.dump(categories, file, indent=4, ensure_ascii=False)
-
-    print("Generated")
+if __name__ == "__main__":
+    world = SoundVoltexWorld()
 
     world_file_name = "manual_SDVX_MapleLeaf"
     script_dir = Path(__file__).parent
@@ -275,6 +278,24 @@ if __name__ == "__main__":
 
     zip_path = apworld_folder / f"{world_file_name}.apworld"
 
+    print(f"Generated {world.item_count} items")
+    print(f"Generated {len(world.locations)} locations")
+    print(f"Configured {len(world.categories)} categories")
+    print(f"World path: {zip_path}")
+
+    print("Saving items...")
+    with open(manual_data_path("items.json"), "w", encoding="utf-8") as file:
+        json.dump(world.items, file, indent="\t", ensure_ascii=False)
+
+    print("Saving locations...")
+    with open(manual_data_path("locations.json"), "w", encoding="utf-8") as file:
+        json.dump(world.locations, file, indent="\t", ensure_ascii=False)
+
+    print("Saving categories...")
+    with open(manual_data_path("categories.json"), "w", encoding="utf-8") as file:
+        json.dump(world.categories, file, indent="\t", ensure_ascii=False)
+
+    print("Saving apworld...")
     with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
         manual_src = script_dir.parent / "src"
         for root, dirs, files in os.walk(manual_src):
@@ -283,4 +304,4 @@ if __name__ == "__main__":
                 arcname = Path(world_file_name) / file_path.relative_to(manual_src)
                 zip_file.write(file_path, arcname)
 
-    print("World built")
+    print("Done!")
